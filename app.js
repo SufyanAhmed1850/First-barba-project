d6abcbd789f2f4d3a20619bdb9231a8a9c2542c8
SufyanAhmed1850 <sufyan.ahmed1850@gmail.com>
2023-07-06 14:20:49 +0500

    content animation changes

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ const pageTransition = () => {
         {
             "clip-path": "polygon(100% 0%, 0% 0%, 0% 100%, 100% 100%)",
             ease: "power4.out",
-            duration: 1.5,
+            duration: 1.425,
         }
     );
     tl.to(".square", {
         "clip-path": "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)",
         ease: "power4.out",
-        duration: 1.5,
+        duration: 1.4,
     });
 };
 
 const contentAnimation = () => {
-    gsap.from(".h", { y: 50, ease: "power4.out", duration: .75 });
+    gsap.from(".h", { y: 50, opacity: 0, duration: .75 });
 };
 
 const delay = (n) => {
